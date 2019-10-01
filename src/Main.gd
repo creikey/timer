3910c8ac48ab322c294ee6cafa4a065aec26dfbf
Cameron Reikes <cameronreikes@gmail.com>
2019-09-30 17:50:17 -0700

    Add basic config file reading project

---
+++ src/Main.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+const config = preload("res://config.tres")
+
+const config_folder_name = "timer"
+onready var config_folder = OS.get_environment("HOME") + "/.config/"
+const config_filename = "theme.ini"
+onready var config_filename_folder = config_folder + config_folder_name
+onready var config_path = config_folder + config_folder_name + "/" + config_filename
+
+func _ready():
+	# load config file
+	var config_file_path: String = ""
+	if OS.get_name() == "X11":
+		
+		# open config directory
+		var config_directory = Directory.new()
+		if config_directory.open(config_folder) == OK:
+			config_directory.list_dir_begin()
+		else:
+			printerr("Could not open directory: ", config_folder)
+			get_tree().quit()
+			return
+	
+		# scan for this application's config folder
+		var file_name = config_directory.get_next()
+		var found: bool = false
+		while (file_name != ""):
+			if config_directory.current_is_dir() and file_name == config_folder_name:
+				found = true
+				break
+			file_name = config_directory.get_next()
+		
+		# make config directory
+		if found:
+			pass
+		else:
+			if config_directory.make_dir(config_folder_name) != OK:
+				printerr("Could not make config file directory '" + config_folder_name + "' in '" + config_folder + "'")
+				get_tree().quit()
+				return
+		
+		# make config file if doesn't exist
+		var config_file = File.new()
+		if config_file.file_exists(config_path):
+			if not config.read_config(config_path):
+				get_tree().quit()
+				return
+		else:
+			if not config.populate_config_file_with_defaults(config_path):
+				get_tree().quit()
+				return
---
+++ src/config.gd
@@ -0,0 +1,18 @@
+extends Resource
+
+var config: ConfigFile
+
+func populate_config_file_with_defaults(in_path: String) -> bool: # returns if successful
+	config = ConfigFile.new()
+	config.set_value("theme", "background_color", ProjectSettings.get("rendering/environment/default_clear_color"))
+	if config.save(in_path) != OK:
+		printerr("Failed to populate default config at '" + in_path + "'")
+		return false
+	return true
+
+func read_config(in_path: String) -> bool:
+	config = ConfigFile.new()
+	if config.load(in_path) != OK:
+		printerr("Failed to load config from '" + in_path + "'")
+		return false
+	return true
